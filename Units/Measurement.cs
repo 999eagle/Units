d101f25ea94f9d4bd86b51c573ee06e6dd8452e9
Erik Tauchert <999eagle@999eagle.moe>
2017-09-02 16:14:11 +0200

    Add operators to calculate with measurements

--- Units/Measurement.cs
+++ Units/Measurement.cs
@@ -43,5 +43,51 @@ namespace Units
 			var conversion = Unit.ConvertTo(newUnit);
 			return new Measurement(Value * conversion.scale + conversion.shift, newUnit);
 		}
+
+		public Measurement ConvertTo(string newUnit)
+		{
+			if (!Unit.TryParseUnit(newUnit, out var unit)) throw new Exception();
+			return ConvertTo(unit);
+		}
+
+		public static Measurement operator *(Measurement lhs, Measurement rhs)
+		{
+			return new Measurement(lhs.Value * rhs.Value, lhs.Unit * rhs.Unit);
+		}
+
+		public static Measurement operator /(Measurement lhs, Measurement rhs)
+		{
+			return new Measurement(lhs.Value / rhs.Value, lhs.Unit / rhs.Unit);
+		}
+
+		public static Measurement operator +(Measurement lhs, Measurement rhs)
+		{
+			if (!lhs.Unit.CanConvertTo(rhs.Unit)) throw new Exception();
+			if (lhs.Unit == rhs.Unit)
+			{
+				return new Measurement(lhs.Value + rhs.Value, lhs.Unit);
+			}
+			else if (rhs.Unit.Shift == 0 && lhs.Unit.Shift != 0)
+			{
+				var converted = lhs.ConvertTo(rhs.Unit);
+				return new Measurement(converted.Value + rhs.Value, rhs.Unit);
+			}
+			else
+			{
+				var converted = rhs.ConvertTo(lhs.Unit);
+				return new Measurement(lhs.Value + converted.Value, lhs.Unit);
+			}
+		}
+
+		public static Measurement operator -(Measurement lhs, Measurement rhs)
+		{
+			if (!lhs.Unit.CanConvertTo(rhs.Unit)) throw new Exception();
+			return lhs + (-rhs);
+		}
+
+		public static Measurement operator -(Measurement rhs)
+		{
+			return new Measurement(-rhs.Value, rhs.Unit);
+		}
 	}
 }
