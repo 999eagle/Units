87ec72e58ac3f2742c60b75692c1de19296d8929
Erik Tauchert <999eagle@999eagle.moe>
2017-09-02 23:36:59 +0200

    Add conversion tests

---
+++ UnitsTests/ConversionTests.cs
@@ -0,0 +1,43 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Units;
+
+namespace UnitsTests
+{
+	[TestClass]
+	public class ConversionTests
+	{
+		[TestMethod]
+		public void TestTemperature()
+		{
+			var c = new Measurement(-40, Unit.Celsius);
+			var f = new Measurement(-40, Unit.Fahrenheit);
+
+			Assert.AreEqual(f, c.ConvertTo(Unit.Fahrenheit));
+
+			var converted = new Measurement(90, Unit.Fahrenheit).ConvertTo(Unit.Celsius).Value;
+			if (Math.Abs(converted - 32.22222222) > 0.000001) { Assert.Fail(); }
+		}
+
+		[TestMethod]
+		public void TestVelocity()
+		{
+			var kmh = new Measurement(36, Unit.Kilometer / Unit.Hour);
+			var ms = new Measurement(10, Unit.MetersPerSecond);
+
+			Assert.AreEqual(10, kmh.ConvertTo(Unit.MetersPerSecond).Value);
+			Assert.AreEqual(36, ms.ConvertTo(Unit.Kilometer / Unit.Hour).Value);
+		}
+
+		[TestMethod]
+		public void TestConvertability()
+		{
+			Assert.IsTrue(Unit.Hour.CanConvertTo(Unit.Second));
+			Assert.IsFalse(Unit.Radians.CanConvertTo(Unit.Scalar));
+			Assert.IsFalse(Unit.SquareMeter.CanConvertTo(Unit.Meter));
+			Assert.IsTrue(Unit.SquareMeter.CanConvertTo(Unit.Furlong * Unit.Foot));
+			Assert.IsTrue(Unit.Siemens.CanConvertTo(Unit.Ampere / Unit.Volt));
+		}
+	}
+}
