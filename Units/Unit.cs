cb96e42e48b4502e12ffb8016d59899688ae6c06
Erik Tauchert <999eagle@999eagle.moe>
2017-09-02 12:09:42 +0200

    Add Unit struct for handling all units (SI basic and derived)

---
+++ Units/Unit.cs
@@ -0,0 +1,64 @@
+using System;
+
+namespace Units
+{
+	public partial struct Unit
+	{
+		public string Name { get; }
+		public Dimension Dimension { get; }
+		public Ratio Scale { get; }
+		public Ratio Shift { get; }
+
+		public Unit(Dimension dimension, Ratio scale, Ratio shift) : this("", dimension, scale, shift) { }
+
+		public Unit(string name, Dimension dimension) : this(name, dimension, new Ratio(1)) { }
+		public Unit(string name, Dimension dimension, Ratio scale) : this(name, dimension, scale, new Ratio(0)) { }
+		public Unit(string name, Dimension dimension, Ratio scale, Ratio shift)
+		{
+			Name = name;
+			Dimension = dimension;
+			Scale = scale;
+			Shift = shift;
+		}
+
+		public Unit(string name, Unit baseUnit, Ratio scale) : this(name, baseUnit, scale, new Ratio(0)) { }
+		public Unit(string name, Unit baseUnit, Ratio scale, Ratio shift)
+		{
+			Name = name;
+			Dimension = baseUnit.Dimension;
+			Scale = baseUnit.Scale * scale;
+			Shift = baseUnit.Shift * scale + shift;
+		}
+
+		public static Unit operator *(Unit lhs, Unit rhs)
+		{
+			if (lhs.Shift != 0 || rhs.Shift != 0) throw new Exception();
+			return GetKnownUnit(lhs.Dimension + rhs.Dimension, lhs.Scale * rhs.Scale, new Ratio(0));
+		}
+
+		public static Unit operator /(Unit lhs, Unit rhs)
+		{
+			if (lhs.Shift != 0 || rhs.Shift != 0) throw new Exception();
+			return GetKnownUnit(lhs.Dimension - rhs.Dimension, lhs.Scale / rhs.Scale, new Ratio(0));
+		}
+
+		public static bool operator ==(Unit lhs, Unit rhs) => lhs.Equals(rhs);
+		public static bool operator !=(Unit lhs, Unit rhs) => !lhs.Equals(rhs);
+		public bool Equals(Unit other) => Dimension == other.Dimension && Scale == other.Scale && Shift == other.Shift;
+		public override bool Equals(object obj) => obj is Unit unit ? Equals(unit) : base.Equals(obj);
+
+		public override int GetHashCode()
+		{
+			return Dimension.GetHashCode() ^ Util.ShiftAndWrap(Scale.GetHashCode(), 4) ^ Util.ShiftAndWrap(Shift.GetHashCode(), 8);
+		}
+
+		public bool CanConvertTo(Unit other) => other.Dimension == Dimension;
+
+		public (Ratio scale, Ratio shift) ConvertTo(Unit other)
+		{
+			if (!CanConvertTo(other)) { throw new Exception(); }
+			var scale = other.Scale / Scale;
+			return (scale, other.Shift - Shift * scale);
+		}
+	}
+}
---
+++ Units/Units.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Units
+{
+	public partial struct Unit
+	{
+		public static readonly Unit Meter = new Unit("m", Dimension.LengthDimension);
+		public static readonly Unit Kilogram = new Unit("kg", Dimension.MassDimension);
+		public static readonly Unit Second = new Unit("s", Dimension.TimeDimension);
+		public static readonly Unit Ampere = new Unit("A", Dimension.CurrentDimension);
+		public static readonly Unit Kelvin = new Unit("K", Dimension.TemperatureDimension);
+		public static readonly Unit Mole = new Unit("mol", Dimension.SubstanceDimension);
+		public static readonly Unit Candela = new Unit("cd", Dimension.IntensityDimension);
+
+		public static readonly Unit Kilometer = new Unit("km", Meter, new Ratio(1000));
+
+		public static readonly Unit Celsius = new Unit("°C", Kelvin, new Ratio(1), new Ratio(-27315, 100));
+		public static readonly Unit Fahrenheit = new Unit("°F", Celsius, new Ratio(9, 5), new Ratio(32));
+
+		public static readonly Unit SquareMeter = Meter * Meter;
+		public static readonly Unit CubicMeter = Meter * Meter * Meter;
+
+		public static readonly Unit MetersPerSecond = Meter / Second;
+
+		public static readonly Unit Newton = new Unit("N", Kilogram * Meter / (Second * Second), new Ratio(1));
+
+		private static IList<Unit> knownUnits =
+			typeof(Unit).GetFields()
+			.Where(f => f.IsStatic && f.IsPublic && f.FieldType == typeof(Unit))
+			.Select(f => (Unit)f.GetValue(null))
+			.ToList();
+
+		public static Unit GetUnitForDimension(Dimension dim)
+		{
+			if (knownUnits == null) new Unit("", dim);
+			var units = knownUnits.Where(u => u.Dimension == dim);
+			if (units.Any())
+			{
+				var unit = units.FirstOrDefault(u => u.Scale == 1 && u.Shift == 0);
+				if (unit.Dimension == dim) return unit;
+				else return units.First();
+			}
+			else
+			{
+				return new Unit("", dim);
+			}
+		}
+
+		public static Unit GetKnownUnit(Dimension dim, Ratio scale, Ratio shift)
+		{
+			if (knownUnits == null) return new Unit(dim, scale, shift);
+			var knownUnit = knownUnits.FirstOrDefault(u => u.Dimension == dim && u.Scale == scale && u.Shift == shift);
+			if (knownUnit.Dimension == dim) return knownUnit;
+			else return new Unit(dim, scale, shift);
+		}
+
+		public static Unit GetKnownUnit(Unit unit)
+		{
+			if (knownUnits == null) return unit;
+			var knownUnit = knownUnits.FirstOrDefault(u => u == unit);
+			if (knownUnit.Dimension == unit.Dimension) return knownUnit;
+			else return unit;
+		}
+	}
+}
