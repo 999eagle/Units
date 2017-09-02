d51d202992f517826905c3786340d09a49ea027b
Erik Tauchert <999eagle@999eagle.moe>
2017-09-02 14:34:06 +0200

    Add a whole load of derived dimensions and units

--- Units/Dimensions.cs
+++ Units/Dimensions.cs
@@ -6,7 +6,9 @@ namespace Units
 {
 	public partial struct Dimension
 	{
+		// Basic dimensions
 		public static readonly Dimension ScalarDimension = new Dimension();
+
 		public static readonly Dimension LengthDimension = new Dimension(length: 1);
 		public static readonly Dimension MassDimension = new Dimension(mass: 1);
 		public static readonly Dimension TimeDimension = new Dimension(time: 1);
@@ -15,5 +17,23 @@ namespace Units
 		public static readonly Dimension SubstanceDimension = new Dimension(substance: 1);
 		public static readonly Dimension LuminousIntensityDimension = new Dimension(luminousIntensity: 1);
 		public static readonly Dimension AngleDimension = new Dimension(angle: 1);
+
+		// Dimenions of derived units
+		public static readonly Dimension SolidAngleDimension = AngleDimension * AngleDimension;
+		public static readonly Dimension FrequencyDimension = ScalarDimension / TimeDimension;
+		public static readonly Dimension ForceDimension = MassDimension * LengthDimension / (TimeDimension ^ 2);
+		public static readonly Dimension PressureDimension = ForceDimension / (LengthDimension ^ 2);
+		public static readonly Dimension EnergyDimension = ForceDimension * LengthDimension;
+		public static readonly Dimension PowerDimension = EnergyDimension / TimeDimension;
+		public static readonly Dimension ChargeDimension = TimeDimension * CurrentDimension;
+		public static readonly Dimension VoltageDimension = PowerDimension / CurrentDimension;
+		public static readonly Dimension CapacitanceDimension = ChargeDimension / VoltageDimension;
+		public static readonly Dimension ResistanceDimension = VoltageDimension / CurrentDimension;
+		public static readonly Dimension ConductanceDimension = CurrentDimension / VoltageDimension;
+		public static readonly Dimension MagneticFluxDimension = VoltageDimension * TimeDimension;
+		public static readonly Dimension MagneticFluxDensityDimension = MagneticFluxDimension / (LengthDimension ^ 2);
+		public static readonly Dimension InductanceDimension = MagneticFluxDimension / CurrentDimension;
+		public static readonly Dimension LuminousFluxDimension = LuminousIntensityDimension * SolidAngleDimension;
+		public static readonly Dimension IlluminanceDimension = LuminousFluxDimension / (LengthDimension ^ 2);
 	}
 }
--- Units/Units.cs
+++ Units/Units.cs
@@ -7,6 +7,7 @@ namespace Units
 {
 	public partial struct Unit
 	{
+		// Basic units
 		public static readonly Unit Meter = new Unit("m", Dimension.LengthDimension);
 		public static readonly Unit Kilogram = new Unit("kg", Dimension.MassDimension);
 		public static readonly Unit Second = new Unit("s", Dimension.TimeDimension);
@@ -16,17 +17,51 @@ namespace Units
 		public static readonly Unit Candela = new Unit("cd", Dimension.LuminousIntensityDimension);
 		public static readonly Unit Radians = new Unit("rad", Dimension.AngleDimension);
 
-		public static readonly Unit Kilometer = new Unit("km", Meter, new Ratio(1000));
+		// Derived units with special names
+		public static readonly Unit Steradian = new Unit("sr", Dimension.SolidAngleDimension);
+		public static readonly Unit Hertz = new Unit("Hz", Dimension.FrequencyDimension);
+		public static readonly Unit Newton = new Unit("N", Dimension.ForceDimension);
+		public static readonly Unit Pascal = new Unit("Pa", Dimension.PressureDimension);
+		public static readonly Unit Joule = new Unit("J", Dimension.EnergyDimension);
+		public static readonly Unit Watt = new Unit("W", Dimension.PowerDimension);
+		public static readonly Unit Coulomb = new Unit("C", Dimension.ChargeDimension);
+		public static readonly Unit Volt = new Unit("V", Dimension.VoltageDimension);
+		public static readonly Unit Farad = new Unit("F", Dimension.CapacitanceDimension);
+		public static readonly Unit Ohm = new Unit("Ω", Dimension.ResistanceDimension);
+		public static readonly Unit Siemens = new Unit("S", Dimension.ConductanceDimension);
+		public static readonly Unit Weber = new Unit("Wb", Dimension.MagneticFluxDimension);
+		public static readonly Unit Tesla = new Unit("T", Dimension.MagneticFluxDensityDimension);
+		public static readonly Unit Henry = new Unit("H", Dimension.InductanceDimension);
+		public static readonly Unit Lumen = new Unit("lm", Dimension.LuminousFluxDimension);
+		public static readonly Unit Lux = new Unit("lx", Dimension.IlluminanceDimension);
 
+		// Length Units
+		public static readonly Unit Centimeter = new Unit("cm", Meter, new Ratio(1, 100));
+		public static readonly Unit Kilometer = new Unit("km", Meter, new Ratio(1000));
+		public static readonly Unit Inch = new Unit("in", Centimeter, new Ratio(254, 100));
+		public static readonly Unit Foot = new Unit("ft", Inch, new Ratio(12));
+		public static readonly Unit Yard = new Unit("yd", Foot, new Ratio(3));
+		public static readonly Unit Rod = new Unit("rd", Foot, new Ratio(165, 10));
+		public static readonly Unit Chain = new Unit("ch", Rod, new Ratio(4));
+		public static readonly Unit Furlong = new Unit("fur", Chain, new Ratio(10));
+
+		// Time Units
+		public static readonly Unit Minute = new Unit("min", Second, new Ratio(60));
+		public static readonly Unit Hour = new Unit("h", Minute, new Ratio(60));
+		public static readonly Unit Day = new Unit("d", Hour, new Ratio(24));
+		public static readonly Unit Week = new Unit("wk", Day, new Ratio(7));
+		public static readonly Unit Fortnight = new Unit("fn", Week, new Ratio(2));
+		public static readonly Unit Year = new Unit("a", Day, new Ratio(365));
+
+		// Temperature units
 		public static readonly Unit Celsius = new Unit("°C", Kelvin, new Ratio(1), new Ratio(-27315, 100));
 		public static readonly Unit Fahrenheit = new Unit("°F", Celsius, new Ratio(9, 5), new Ratio(32));
+		public static readonly Unit Rankine = new Unit("°R", Kelvin, new Ratio(9, 5));
 
+		// Other stuff
 		public static readonly Unit SquareMeter = Meter * Meter;
 		public static readonly Unit CubicMeter = Meter * Meter * Meter;
-
 		public static readonly Unit MetersPerSecond = Meter / Second;
-
-		public static readonly Unit Newton = new Unit("N", Kilogram * Meter / (Second * Second), new Ratio(1));
 
 		private static IList<Unit> knownUnits =
 			typeof(Unit).GetFields()
